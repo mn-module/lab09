9a0abae79ff513120d3f1e021bcb592f2218e14b
WAI PHYO HTET <66250792+mn-module@users.noreply.github.com>
2026-05-31 18:50:51 +0800

    Update search_result.php

--- search_result.php
+++ search_result.php
@@ -1,43 +1,54 @@
 <?php
-    include "setting.php";
+    require_once("setting.php");
 
-    if (isset($_GET['model'])) {
-        $model = mysqli_real_escape_string($conn, $_GET['model']);
-
-        // FIXED SQL QUERY
-        $sql = "SELECT * FROM cars WHERE model LIKE '%$model%'";
-        $result = mysqli_query($conn, $sql);
-
-        if (mysqli_num_rows($result) > 0) {
-            echo "<table border='1' cellpadding='5'>";
-
-            echo "
-                <tr>
-                    <th>ID</th>
-                    <th>Make</th>
-                    <th>Model</th>
-                    <th>Price</th>
-                    <th>Year</th>
-                </tr>
-            ";
-
-            while ($row = mysqli_fetch_assoc($result)) {
-                echo "<tr>";
-                echo "<td>" . $row['car_id'] . "</td>";
-                echo "<td>" . $row['make'] . "</td>";
-                echo "<td>" . $row['model'] . "</td>";
-                echo "<td>" . $row['price'] . "</td>";
-                echo "<td>" . $row['yom'] . "</td>";
-                echo "</tr>";
-            }
-
-            echo "</table>";
-        } else {
-            echo " No matching cars found.";
-        }
-    } else {
-        echo "Please enter a model to search.";
+    // Check if the model was sent from the search form
+    if (!isset($_GET['model'])) {
+        die("Please search from search_form.php");
     }
 
-    mysqli_close($conn);
+    // Clean the user input before using it in the SQL query
+    $model = mysqli_real_escape_string($dbconn, $_GET['model']);
+
+    // Search for cars where the model contains the entered text
+    $query = "SELECT car_id, make, model, price, yom 
+              FROM cars 
+              WHERE model LIKE '%$model%'";
+
+    // Run the query
+    $result = mysqli_query($dbconn, $query);
+
+    // Check if matching cars were found
+    if ($result && mysqli_num_rows($result) > 0) {
+        echo "<table border='1'>";
+
+        echo "
+            <tr>
+                <th>Car ID</th>
+                <th>Make</th>
+                <th>Model</th>
+                <th>Price</th>
+                <th>YOM</th>
+            </tr>
+        ";
+
+        // Display each matching car in a table row
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo "
+                <tr>
+                    <td>{$row['car_id']}</td>
+                    <td>{$row['make']}</td>
+                    <td>{$row['model']}</td>
+                    <td>{$row['price']}</td>
+                    <td>{$row['yom']}</td>
+                </tr>
+            ";
+        }
+
+        echo "</table>";
+    } else {
+        echo "<p>No cars found.</p>";
+    }
+
+    // Close the database connection
+    mysqli_close($dbconn);
 ?>
