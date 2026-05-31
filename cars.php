0fc0472582b708b7cc9760fbd51e1c2f6ead40b2
WAI PHYO HTET <66250792+mn-module@users.noreply.github.com>
2026-05-31 18:48:13 +0800

    Update cars.php

--- cars.php
+++ cars.php
@@ -10,14 +10,19 @@
     <h1>Car Exhibition List</h1>
 
     <?php
-        require_once("settings.php");
+        require_once("setting.php");
 
+        // Connect to the database
         $dbconn = @mysqli_connect($host, $user, $pwd, $sql_db);
 
         if ($dbconn) {
+            // Prepare the SQL query
             $query = "SELECT car_id, make, model, price, yom FROM cars";
+
+            // Run the query
             $result = mysqli_query($dbconn, $query);
 
+            // Check if cars were found
             if ($result && mysqli_num_rows($result) > 0) {
                 echo "<table border='1'>";
 
@@ -31,23 +36,28 @@
                     </tr>
                 ";
 
+                // Display each car in a table row
                 while ($row = mysqli_fetch_assoc($result)) {
-                    echo "<tr>";
-                    echo "<td>" . $row['car_id'] . "</td>";
-                    echo "<td>" . $row['make'] . "</td>";
-                    echo "<td>" . $row['model'] . "</td>";
-                    echo "<td>" . $row['price'] . "</td>";
-                    echo "<td>" . $row['yom'] . "</td>";
-                    echo "</tr>";
+                    echo "
+                        <tr>
+                            <td>{$row['car_id']}</td>
+                            <td>{$row['make']}</td>
+                            <td>{$row['model']}</td>
+                            <td>{$row['price']}</td>
+                            <td>{$row['yom']}</td>
+                        </tr>
+                    ";
                 }
 
                 echo "</table>";
 
+                // Free result memory
                 mysqli_free_result($result);
             } else {
                 echo "<p>There are no cars to display.</p>";
             }
 
+            // Close database connection
             mysqli_close($dbconn);
         } else {
             echo "<p>Unable to connect to the db.</p>";
